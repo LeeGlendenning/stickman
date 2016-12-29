7616e12d1b24f66a7271856ae3e77d6c28bdc107
Lee <theboss@TheBosss-Mac-mini.local>
2016-12-29 11:11:03 -0500

    Add Fist class

---
+++ Fist.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stickman;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author theboss
+ */
+public class Fist implements iWeapon{
+    
+    public Fist() {
+        
+    }
+    
+    @Override
+    public void draw(Graphics g, int x, int y) {
+        System.out.println("No weapon");
+    }
+    
+}
